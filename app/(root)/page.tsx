3d441c01ebdc441b96aa22842b60f85e729f1bd5
Daniel Okello <43291086+okellodaniel@users.noreply.github.com>
2023-07-02 12:57:06 +0300

    Added clerk authentication

---
+++ app/(root)/page.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button"
+
+const Home = () => {
+    return (
+        <>
+            <div className="p-4">
+                <p> Hello Admin Dashboard</p>
+            </div>
+            <div className="p-4">
+                <Button variant="outline">
+                    Click Me
+                </Button>
+            </div>
+        </>
+    )
+}
+
+export default Home
