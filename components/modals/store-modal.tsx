e2a17e37a19270c987e8e1deaece3a90d770e199
Daniel Okello <43291086+okellodaniel@users.noreply.github.com>
2023-07-09 23:37:09 +0300

    Added missing logic for pop up form

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -3,11 +3,13 @@
 import * as z from "zod";
 
 import { useStoreModal } from "@/hooks/use-store-modal"
-import { Modal } from "../ui/modal"
+import { Modal } from "@/components/ui/modal"
 import { useForm } from "react-hook-form";
 
 import { zodResolver } from "@hookform/resolvers/zod"
-import { Form } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const formSchema = z.object({ name: z.string().min(1) });
 
@@ -34,9 +36,31 @@ export const StoreModal = () => {
             onClose={storeModal.onClose}
         >
             <div className="space-y-4 py-2 pb-4">
-                <Form></Form>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)}>
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Name
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="E-Commerce" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <div className="pt-6 space-x-2 flex items-center justify-end">
+                            <Button variant="outline" onClick={storeModal.onClose}>close</Button>
+                            <Button type="submit">Continue</Button>
+                        </div>
+                    </form>
+                </Form>
             </div>
         </Modal>
-    )
+    );
 
 }
