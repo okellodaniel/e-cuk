40dd02b363b9d7a43bbb6f0ffcda098f927a2a65
Daniel Okello <43291086+okellodaniel@users.noreply.github.com>
2023-11-18 22:52:56 +0300

    Created the settings and swtch store components

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,8 +1,37 @@
 "use client"
 
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+
 const MainNav = ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => {
+
+    const params = useParams();
+    const pathname = usePathname();
+
+    const routes = [{
+        href: `/${params.storeId}/settings`,
+        label: 'Settings',
+        active: pathname === `/${params.storeId}/settings`
+    }];
+
     return (
-        <p>Main Navigation</p>
+        <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)} {...props}>
+
+            {routes.map((route) => (
+                <Link
+                    key={route.href}
+                    href={route.href}
+                    className={cn("text-sm font-medium, transition-colors hover:text-primary",
+                        route.active ? "text-black dark:text-white" : "text-muted-foreground")}
+                >
+                    {route.label}
+                    
+                </Link>
+            ))}
+
+        </nav>
     );
 };
 
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,27 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth, redirectToSignIn } from "@clerk/nextjs";
 import MainNav from "@/components/main-nav";
+import StoreSwitcher from "@/components/store-switcher";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
 
-const NavBar = () => {
+const NavBar = async () => {
+
+    const { userId } = auth();
+    if (!userId) redirect("/sign-in");
+
+    const stores = await prismadb.store.findMany(
+        {
+            where: {
+                userId
+            }
+        }
+    );
+
     return (
         <div className="border-b">
             <div className="flex h-16 items-center px-4">
-                <div>This will be a swticher</div>
-                <MainNav />
+                <StoreSwitcher items={stores} />
+                <MainNav className="mx-6" />
                 <div className="ml-auto flex items-center space-x-4">
                     <UserButton afterSignOutUrl="/" />
                 </div>
