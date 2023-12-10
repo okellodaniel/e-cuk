4dbfeb924c7d081dad994d63e4b9c274d1f8d4f7
Daniel Okello <43291086+okellodaniel@users.noreply.github.com>
2023-12-10 21:46:42 +0300

    removed unused imports

--- app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -1,8 +1,6 @@
 "use client"
 import { useParams, useRouter } from "next/navigation"
-import { Plus } from "lucide-react"
 
-import { Button } from "@/components/ui/button"
 import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
 import { OrderColumn, columns } from "./columns"
